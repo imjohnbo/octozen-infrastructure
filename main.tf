ed849142aba809e9dbfb5380c1057a8efb2c8057
John Bohannon <imjohnbo@github.com>
2019-11-08 10:26:03 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "remote" {
+    workspaces {
+      name = "octozen"
+    }
+  }
+}
+
+variable "netlify_token" {
+  type = "string"
+}
+
+# Configure the Netlify Provider
+provider "netlify" {
+  token    = "${var.netlify_token}"
+}
+
+# Define your site
+resource "netlify_site" "main" {
+  name = "vigilant-meme"
+
+  repo {
+    repo_branch   = "master"
+    repo_path     = "imjohnbo/octozen"
+    provider      = "github"
+  }
+}
